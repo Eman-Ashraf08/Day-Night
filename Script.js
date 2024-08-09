331dbc0bc2daf3a7749c885d7c6d8b22fc302e11
EfpaEmi <efpaemi@gmail.com>
2024-08-09 14:18:23 +0500

    mini JS project

---
+++ Script.js
@@ -0,0 +1,9 @@
+function circle_Button() {
+    var rectangle = document.querySelector('.rectangle');
+    rectangle.classList.toggle('move-right');
+    rectangle.classList.toggle('change-image');
+    var color = document.body
+    color.classList.toggle('change-color')
+    var circle = document.querySelector('.circle')
+    circle.classList.toggle('change-bgColor')
+}
